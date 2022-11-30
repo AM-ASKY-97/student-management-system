8914d83f6281c23c1791b4eaa3f0e6686603a1cc
AM-ASKY <am.asky97@gmail.com>
2022-11-30 23:21:31 +0530

    Student Management System

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container-fluid bg-primary p-3">
+        <h1 class="text-white text-center">Student Management System</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 mt-3">
+                <form action="">
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="" class="form-label">First Name</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+
+                        <div class="col-6">
+                            <label for="" class="form-label">Last Name</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="" class="form-label">Email</label>
+                            <input type="email" name="" id="" class="form-control">
+                        </div>
+
+                        <div class="col-6">
+                            <label for="" class="form-label">Phone Number</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <label for="" class="form-label">Address</label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                    </div>
+
+                    <input type="submit" value="submit" class="btn btn-success">
+                    
+                </form>
+            </div>
+
+            <div class="col-6 mt-3">
+                <img src="https://postingtip.com/wp-content/uploads/2022/07/assignmenthelp.jpg" alt="" class="img-fluid rounded">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
